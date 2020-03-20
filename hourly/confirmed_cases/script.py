81722225238c04ee5f7048341b472c8db002d90f
krsh2001 <dynosa,ead@gmail.com>
2020-03-20 11:14:14 -0400

    split script into functions

--- hourly/confirmed_cases/script.py
+++ hourly/confirmed_cases/script.py
@@ -13,8 +13,9 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1D6okqtBS3S2NRC7GFVHzaZ67DuTw7LX49-fqSLwJyeo'
 SAMPLE_RANGE_NAME = 'A1:AA1000'
+output = {}
 
-def main():
+def get_spreadsheet_data():
     global values_input, service
     creds = None
     if os.path.exists('token.pickle'):
@@ -41,57 +42,58 @@ def main():
     if not values_input and not values_expansion:
         print('No data found.')
 
-main()
 
-df=pd.DataFrame(values_input)
+def geocode_sheet():
+    df=pd.DataFrame(values_input)
 
-last_updated = df[0][0]
-df = df.drop([0, 1])
+    global last_updated
+    last_updated = df[0][0]
 
-column_names = []
+    df = df.drop([0, 1])
 
-for item in df.iloc[0]:
-    column_names.append(item)
+    column_names = []
 
-df.columns = column_names
-df.index = df.index - 2
-df = df.drop(0)
+    for item in df.iloc[0]:
+        column_names.append(item)
 
-geolocator = Nominatim(user_agent="COVIDScript")
+    df.columns = column_names
+    df.index = df.index - 2
+    df = df.drop(0)
 
-output = {}
+    geolocator = Nominatim(user_agent="COVIDScript")
 
-name_exceptions = {"Kingston Frontenac Lennox & Addington": "Kingston", 
-                    "Zone 2 (Saint John area)": "Saint John"}
+    name_exceptions = {"Kingston Frontenac Lennox & Addington": "Kingston", 
+                        "Zone 2 (Saint John area)": "Saint John"}
 
 
-for index, row in df.iterrows():
-    if row['health_region'] + ', ' + row['province'] in output:
-        output[row['health_region'] + ', ' + row['province']][0] += 1
-    else:
-        if row['health_region'] == "Not Reported" and row['province'] == "Repatriated":
-            output[row['health_region'] + ', ' + row['province']] = [1, "N/A", "N/A"]
-        elif row['health_region'] == "Not Reported":
-            location = geolocator.geocode(row['province'] + ', Canada')
-            output[row['health_region'] + ', ' + row['province']] = [1, location.latitude, location.longitude]
+    for index, row in df.iterrows():
+        if row['health_region'] + ', ' + row['province'] in output:
+            output[row['health_region'] + ', ' + row['province']][0] += 1
         else:
-            if row['health_region'] in name_exceptions:
-                location = geolocator.geocode(name_exceptions[row['health_region']] + ', ' + row['province'] + ', Canada')
-            else: 
-                location = geolocator.geocode(row['health_region'] + ', ' + row['province'] + ', Canada')
-                
-            if location is None:
+            if row['health_region'] == "Not Reported" and row['province'] == "Repatriated":
+                output[row['health_region'] + ', ' + row['province']] = [1, "N/A", "N/A"]
+            elif row['health_region'] == "Not Reported":
                 location = geolocator.geocode(row['province'] + ', Canada')
-            output[row['health_region'] + ', ' + row['province']] = [1, location.latitude, location.longitude]
+                output[row['health_region'] + ', ' + row['province']] = [1, location.latitude, location.longitude]
+            else:
+                if row['health_region'] in name_exceptions:
+                    location = geolocator.geocode(name_exceptions[row['health_region']] + ', ' + row['province'] + ', Canada')
+                else: 
+                    location = geolocator.geocode(row['health_region'] + ', ' + row['province'] + ', Canada')
+                    
+                if location is None:
+                    location = geolocator.geocode(row['province'] + ', Canada')
+                output[row['health_region'] + ', ' + row['province']] = [1, location.latitude, location.longitude]
 
-real_output = []
+def output_json():
+    real_output = []
 
-for key in output:
-    real_output.append({"name": key, "cases": output[key][0], "coord": [output[key][1], output[key][2]]})
+    for key in output:
+        real_output.append({"name": key, "cases": output[key][0], "coord": [output[key][1], output[key][2]]})
 
-with open('confirmed.js', 'w') as outfile:
-    output_string = json.dumps(real_output)
-    output_string = output_string.replace("Vancouver Coastal", "Vancouver")
-    output_string = output_string.replace("'", r"\'")
-    outfile.write("data_last_updated = '" + last_updated + "';\n")
-    outfile.write("data_confirmed = '"+output_string + "';")
+    with open('confirmed.js', 'w') as outfile:
+        output_string = json.dumps(real_output)
+        output_string = output_string.replace("Vancouver Coastal", "Vancouver")
+        output_string = output_string.replace("'", r"\'")
+        outfile.write("data_last_updated = '" + last_updated + "';\n")
+        outfile.write("data_confirmed = '"+output_string + "';")
