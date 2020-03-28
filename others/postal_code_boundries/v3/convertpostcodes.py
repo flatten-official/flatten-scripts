73d29cdbba2c06d1a6497a8f9f998e81f46788d9
n-ivan <n-ivan@users.noreply.github.com>
2020-03-27 22:57:42 -0400

    made postal code converter v3

---
+++ others/postal_code_boundries/v3/convertpostcodes.py
@@ -0,0 +1,34 @@
+
+import json
+import pyproj
+
+CONVERT_IN_PROJ = pyproj.Proj('epsg:3347')
+CONVERT_OUT_PROJ = pyproj.Proj('epsg:4326')
+TRANSFORMER = pyproj.Transformer.from_proj(CONVERT_IN_PROJ, CONVERT_OUT_PROJ, always_xy=True)
+
+def convert(filename):
+    with open(filename, 'r') as file:
+        input_data = json.load(file)
+    output_data = input_data
+    for i in range(len(output_data["features"])):
+        #output_data['features'][i] = ["NIZE"]
+        if output_data["features"][i]['geometry']['type'] == 'Polygon':
+            for j in range(len(output_data["features"][i]['geometry']['coordinates'])):
+                coords = []
+                for lng, lat in TRANSFORMER.itransform(output_data["features"][i]['geometry']['coordinates'][j]):
+                    coords.append([lng, lat])
+                output_data["features"][i]['geometry']['coordinates'][j] = coords
+                      
+        elif output_data["features"][i]['geometry']['type'] == 'MultiPolygon':
+            for j in range(len(output_data["features"][i]['geometry']['coordinates'])):
+                for k in range(len(output_data["features"][i]['geometry']['coordinates'][j])):
+                    coords = []
+                    for lng, lat in TRANSFORMER.itransform(output_data["features"][i]['geometry']['coordinates'][j][k]):
+                        coords.append([lng, lat])
+                    output_data["features"][i]['geometry']['coordinates'][j][k] = coords
+
+    with open("converted_boundaries.json", "w") as file:
+        json.dump(output_data, file)
+
+if __name__ == '__main__':
+    convert("unformatted_postal_code_boundaries.json")
