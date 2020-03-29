f33cb725742e665e1efe73b629ac99f231642f5a
n-ivan <n-ivan@users.noreply.github.com>
2020-03-29 15:01:50 -0400

    added travel, death and recovery data

--- hourly/confirmed_cases/main.py
+++ hourly/confirmed_cases/main.py
@@ -5,18 +5,18 @@ Make sure it runs smoothly and reports any errors clearly.
 
 import script
 
-
 def main(event, context):
     print("Getting data from spreadsheet...")
 
-    data = script.get_spreadsheet_data()
+    confirmed, recovered, dead = script.get_spreadsheet_data()
 
     print("Geocoding data...")
 
-    output = script.geocode_sheet(data)
-
+    confirmed_output = script.geocode_sheet(confirmed)
+    travel_data = script.get_travel_data(confirmed)
+    provincial_data = script.get_provincial_totals(confirmed_output, recovered, dead)
     print("Outputting data to file...")
-    script.output_json(output)
+    script.output_json(confirmed_output, travel_data, provincial_data)
     print("Done")
 
 
