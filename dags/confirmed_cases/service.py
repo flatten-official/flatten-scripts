dfdd7af05a0f1d86150cbe976c2340893e42018e
Shahzad Raza <shahzad@squareup.com>
2020-04-20 15:19:11 -0400

    Switched back to FiletoGCS and changed save path to shareable location

--- dags/confirmed_cases/service.py
+++ dags/confirmed_cases/service.py
@@ -63,6 +63,7 @@ SPREADSHEET_CASES = 'Cases'
 SPREADSHEET_REC = 'Recovered'
 SPREADSHEET_DEATH = 'Mortality'
 
+upload_location = '/home/airflow/gcs/data/'
 UPLOAD_CONFIRMED = 'confirmed_data_composer.json'
 UPLOAD_TRAVEL = 'travel_data.json'
 UPLOAD_PROVINCIAL = 'provincial_data.json'
@@ -288,28 +289,17 @@ def get_provincial_totals(output_dict, rec_df, dead_df):
     return provincial_data
 
 
-# Uploads the JSON to the bucket
-def write_data_to_bucket(travel_out, provincial_out):
-    bucket = storage.Client().bucket(GCS_BUCKET)
-
-    helper.upload_json(bucket, travel_out, UPLOAD_TRAVEL)
-    helper.upload_json(bucket, provincial_out, UPLOAD_PROVINCIAL)
-
-
 def main():
     print("Getting data from spreadsheet...")
-
     confirmed, recovered, dead = get_spreadsheet_data()
-
     print("Geocoding data (this takes a few minutes)...")
-
     confirmed_output = get_confirmed_cases(confirmed)
     travel_data = get_travel_data(confirmed)
     provincial_data = get_provincial_totals(confirmed_output, recovered, dead)
     print("Writing files to disk...")
-    write_json_to_disk(confirmed_output, UPLOAD_CONFIRMED)
-    write_json_to_disk(travel_data, UPLOAD_TRAVEL)
-    write_json_to_disk(provincial_data, UPLOAD_PROVINCIAL)
+    write_json_to_disk(confirmed_output, os.path.join(upload_location, UPLOAD_CONFIRMED))
+    write_json_to_disk(travel_data, os.path.join(upload_location, UPLOAD_TRAVEL))
+    write_json_to_disk(provincial_data, os.path.join(upload_location, UPLOAD_PROVINCIAL))
     print("Done")
 
 
