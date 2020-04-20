2e951012a3869dfad4aa8513947e20f703d33f1c
Shahzad Raza <shahzad@squareup.com>
2020-04-20 15:20:39 -0400

    Switched back to FiletoGCS and changed save path to shareable location

--- dags/confirmed_cases_dag.py
+++ dags/confirmed_cases_dag.py
@@ -31,7 +31,7 @@ default_args = {
 }
 
 confirmed_cases_dag = DAG(
-    dag_id='confirmed_cases',
+    dag_id='get_confirmed_cases',
     start_date=datetime(2020, 4, 18),
     schedule_interval='5 */4 * * *',
     default_args=default_args,
