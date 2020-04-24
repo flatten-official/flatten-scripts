5c581ec9b9dc96e469dcce04ffe1ed0cd6cb84fe
William Wen <william.wen360@gmail.com>
2020-04-24 17:56:45 -0400

    Created the graph scripts dag

--- dags/graph_scripts_dag.py
+++ dags/graph_scripts_dag.py
@@ -6,13 +6,11 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.file_to_gcs import FileToGoogleCloudStorageOperator
 
-from confirmed_cases.service import main
+from graph_scripts.icu import main
 
 GCS_BUCKET = os.environ.get('GCS_SAVE_BUCKET')
 upload_location = '/home/airflow/gcs/data'
-confirmed_file = 'confirmed_data_composer.json'
-travel_file = 'travel_data_composer.json'
-provincial_file = 'provincial_data_composer.json'
+icu_file = 'icu_capacity_composer.json'
 
 default_args = {
     'owner': 'Flatten.ca',
@@ -21,8 +19,8 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-confirmed_cases_dag = DAG(
-    dag_id='confirmed_cases',
+graph_scripts_dag = DAG(
+    dag_id='graph_scripts',
     start_date=datetime(2020, 4, 18),
     schedule_interval='5 4,16 * * *',
     default_args=default_args,
@@ -31,13 +29,13 @@ confirmed_cases_dag = DAG(
 
 echo = BashOperator(
     task_id='Echo',
-    bash_command='echo "Running Confirmed Cases scripts"'
+    bash_command='echo "Running Graph scripts"'
 )
 
 run_service = PythonOperator(
-    task_id='get_confirmed_cases',
+    task_id='run_graph_scripts',
     python_callable=main,
-    dag=confirmed_cases_dag
+    dag=graph_scripts_dag
 )
 
 echo >> run_service
