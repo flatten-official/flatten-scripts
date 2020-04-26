697e3fa07bcb10cdd196befb346b3d1f8f8ee271
n-ivan <n-ivan@users.noreply.github.com>
2020-04-26 18:29:03 -0400

    migrated form data

--- dags/sanitisation_dag.py
+++ dags/sanitisation_dag.py
@@ -7,6 +7,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.file_to_gcs import FileToGoogleCloudStorageOperator
 
 from sanitisation.service import main
+from form_data.service import main as main_form
 
 from gcs.debugger import enable_cloud_debugger
 
@@ -27,15 +28,34 @@ sanitisation_dag = DAG(
     catchup=True
 )
 
+form_data_dag = DAG(
+    dag_id='form_data',
+    start_date=datetime(2020,4,22),
+    schedule_interval='*/10 * * * *',
+    default_args=default_args,
+    catchup=True
+)
+
 echo = BashOperator(
     task_id='Echo',
     bash_command='echo "Running Sanitisation Script"'
 )
+echo_form = BashOperator(
+    task_id='Echo',
+    bash_command='echo "Running Form Data Script"'
+)
 
-run_service = PythonOperator(
+sanitise = PythonOperator(
     task_id='sanitisation',
     python_callable=main,
     dag=sanitisation_dag
 )
 
-echo >> run_service
+form = PythonOperator(
+    task_id='form_data',
+    python_callable=main_form,
+    dag=form_data_dag
+)
+
+echo >> sanitise
+echo_form >> form
