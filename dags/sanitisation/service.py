8fde484e4938919872019cce46c49d3a3b229bc3
Martin Staadecker <machstg@gmail.com>
2020-05-03 18:37:07 -0400

    Minor cleanup

--- dags/sanitisation/service.py
+++ dags/sanitisation/service.py
@@ -1,24 +1,11 @@
 from google.cloud import datastore, storage
 import sanitisation.sanitisation
-import io
-import json
 import os
 import time
 import csv
+
+from utils.bucket_functions import upload_blob
 from utils.file_utils import load_excluded_postal_codes, load_keys
-
-def upload_blob(bucket, data_string, destination_blob_name):
-    """Uploads a file to the bucket."""
-
-    blob = bucket.blob(destination_blob_name)
-
-    blob.upload_from_string(data_string)
-
-    print(
-        "File {} uploaded to {}.".format(
-            destination_blob_name, bucket
-        )
-    )
 
 GCS_BUCKETS = os.environ['GCS_BUCKETS'].split(',')
 GCS_PATHS = os.environ['GCS_PATHS'].split(',')
@@ -26,6 +13,7 @@ DS_NAMESPACE = os.environ['DS_NAMESPACE']
 DS_KIND = 'FlattenAccount'
 DS_KIND_PAPERFORM = 'PaperformSubmission'
 END_FILE_NAME = os.environ['END_FILE_NAME']
+
 
 def main():
     """
@@ -63,7 +51,7 @@ def main():
             writer_paperform.writerow(obj)
 
     curr_time_ms = str(int(time.time() * 1000))
-    
+
     for bucket_name, path in zip(GCS_BUCKETS, GCS_PATHS):
         bucket = storage_client.bucket(bucket_name)
         file_name = os.path.join(path, "-".join([curr_time_ms, END_FILE_NAME]))
