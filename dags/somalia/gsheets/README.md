0adbdc969d5f4646c281d7e67cf3f52490adbe56
Martin Staadecker <machstg@gmail.com>
2020-05-04 18:06:44 -0400

    Update README and cleanup

--- dags/somalia/gsheets/README.md
+++ dags/somalia/gsheets/README.md
@@ -2,7 +2,38 @@
 
 This script uploads the data from the Google Datastore for Somalia to a Google Sheets.
 
-## `columns.txt`
+## First time setup
+
+1. Create a GSheet with the correct tabs (tab names match `TAB_NAMES`).
+
+2. Create a service account and share the sheet with the service account.
+
+3. Upload the service account's credentials to the GCP Secret Manager.
+
+## Adding a column after adding a question to the paperform.
+
+Simply add the paperform question's custom pre-fill key to `columns.txt` AT THE END OF THE FILE. DO NOT insert the 
+pre-fill key in the middle of the list and DO NOT change the order of other pre-fill keys.
+
+## Code details
+
+### Code steps
+
+1. Create a query to the GCP DataStore
+
+2. Execute the query and loop through results
+
+3. For each row in the database add the row to a list of rows. This forms the first tab.
+
+4. Format the data (e.g. replacing None with an empty string)
+
+5. Generate the Info tab.
+
+6. Get the credentials required to upload to Google Sheets from the GCP Secret Manager.
+
+7. For each tab, clear the tab and upload the new tab content to the GSheets.
+
+### `columns.txt`
 
 This file specifies the columns in the Google sheet.
 
@@ -16,32 +47,45 @@ This file specifies the columns in the Google sheet.
 
 - `timestamp`, the first column name is an exception and is not actually a field in the paperform. The Python code accounts for this, do not remove this column.
 
-## `excluded_columns.txt`
+### `excluded_columns.txt`
 
 A list of columns (paperform pre-fill custom keys) that should not be added to the GSheet. Order is unimportant. Do not include values that use to be in `columns.txt`.
 
-## Code logic
 
-1. Create a query to the Google DataStore
+### Notes 
 
-2. Execute the query and loop through results
+- The Google Sheets is erased and re-written on every run. Hence, the whole database is read on every run. 
+This ensures that the script is not dependant on it's outputs (the GSheet). Having the one-way stream avoids issues.
 
-3. For each row in the database add the row to a list of rows.
-
-4. Get the credentials required to upload to Google Sheets from the GCP Secret Manager
-
-5. Upload to the Google Sheets the list of rows (now that we have the credentials).
-
-Notes: 
-
-- The Google Sheets is overwritten on every run.
-
-- The whole database is read on every run.
-
-## First time setup
-
-1. Create a GSheet with the correct tabs (tab names match `TAB_NAMES`).
-
-2. Create a service account and share the sheet with the service account.
-
-3. Upload the service account's credentials to the GCP Secret Manager.
+### Documentation of data structure of the data column in an entity of the datastore
+```
+data: {
+    "death_age": {          # Matches custom_key
+      "entityValue": {
+        "properties": {
+          "type": {
+            "stringValue": "number"
+          },
+          "value": {
+            "nullValue": null
+          },
+          "custom_key": {
+            "stringValue": "death_age"
+          },
+          "title": {
+            "stringValue": "Da’da qofka dhintay"
+          },
+          "key": {
+            "stringValue": "6aqpk"
+          },
+          "description": {
+            "nullValue": null
+          }
+        }
+      }
+    },
+    .
+    .
+    .
+  }
+  ```
--- dags/somalia/gsheets/somalia_sheet.py
+++ dags/somalia/gsheets/somalia_sheet.py
@@ -1,34 +1,5 @@
 """
-Documentation of data structure of the data column in an entity of the datastore
-        data: {
-            "death_age": {          # Matches custom_key
-              "entityValue": {
-                "properties": {
-                  "type": {
-                    "stringValue": "number"
-                  },
-                  "value": {
-                    "nullValue": null
-                  },
-                  "custom_key": {
-                    "stringValue": "death_age"
-                  },
-                  "title": {
-                    "stringValue": "Da’da qofka dhintay"
-                  },
-                  "key": {
-                    "stringValue": "6aqpk"
-                  },
-                  "description": {
-                    "nullValue": null
-                  }
-                }
-              }
-            },
-            .
-            .
-            .
-          }
+Read README.md for documentation of code.
 """
 
 import json
@@ -42,11 +13,10 @@ from utils.config import load_name_config, get_project_id
 from utils.secrets import access_secret_version
 from utils.time import get_printable_cur_time
 
-CONFIG = load_name_config('somalia')
-SPREADSHEET_ID = CONFIG['sheet_id']
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 TAB_NAMES = ['Data', "Info"]
-SECRET_ID = "upload-sheets-somalia"
+
+CONFIG = load_name_config('somalia')
 
 # Mapping of column to position. e.g. { timestamp: 0, district_name: 2 , ... }
 COLUMNS = get_column_mapping()
@@ -56,12 +26,16 @@ EXCLUDED_COLUMNS = get_excluded_column_mapping()
 
 
 def main():
-    query_iterator = get_reference_to_data()
-    data_output = parse_data(query_iterator)
-    upload_to_sheets([data_output, get_info_tab()])
+    data_iterator = get_data()
+
+    data_output = parse_data(data_iterator)
+
+    format_data(data_output)
+
+    upload_data([data_output, get_info_tab()])
 
 
-def get_reference_to_data():
+def get_data():
     """Returns an iterator that allows iterating through the elements of the datastore"""
     client = datastore.Client(namespace=CONFIG["ds_namespace"])
     query = client.query(kind=CONFIG["ds_kind"])
@@ -71,12 +45,12 @@ def get_reference_to_data():
 
 def parse_data(iterator):
     """Loops through the data and generates the data tab"""
-
-    output = [[""] * len(COLUMNS)]  # Start with a row for the header
-
     # Create header row
+    header_row = []
     for column_name, col_index in COLUMNS.items():
-        output[0][col_index] = column_name
+        header_row.append(column_name)
+
+    output = [header_row]
 
     timestamp_index = COLUMNS["timestamp"]
 
@@ -91,24 +65,29 @@ def parse_data(iterator):
                 EXCLUDED_COLUMNS[column_name] = True
                 continue
 
-            value = str(datastore_row["data"][column_name]["value"])
-            if value == "None" or value == "[]":  # Paperform fills in None or [] when the question was never answered
-                continue
-
             try:
                 col_index = COLUMNS[column_name]
             except KeyError:
                 raise KeyError(column_name + "was a value in the datastore but not in the expected column list. Add "
                                              "it to columns.txt following README.md")
 
-            output_row[col_index] = value
+            output_row[col_index] = str(datastore_row["data"][column_name]["value"])
 
         output.append(output_row)
 
     return output
 
 
-def upload_to_sheets(data):
+def format_data(data):
+    """Formats the paperform data to be more friendly."""
+    for i, row in enumerate(data):
+        for j, cell in enumerate(row):
+            # Paperform fills in None or [] when the question was never answered
+            if cell == "[]" or cell == "None":
+                data[i][j] = ""
+
+
+def upload_data(data):
     """
     Upload a list of rows to the spreadsheet using the credentials in the GCP Secret Manager
     Useful docs :
@@ -117,15 +96,16 @@ def upload_to_sheets(data):
     """
 
     # Get credentials for Google Sheets API
-    secret_payload = access_secret_version(get_project_id(), SECRET_ID, "latest")
+    secret_payload = access_secret_version(get_project_id(), CONFIG['sheets_service_account_secret_id'], "latest")
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(secret_payload), SCOPES)
+    spreadsheet_id = CONFIG['sheet_id']
 
     # Upload to Google Sheets through the Google Sheets API
     service = build('sheets', 'v4', credentials=credentials)
 
     for tab_name, tab_data in zip(TAB_NAMES, data):
-        clear_request = service.spreadsheets().values().clear(spreadsheetId=SPREADSHEET_ID, range=tab_name)
-        write_request = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=tab_name,
+        clear_request = service.spreadsheets().values().clear(spreadsheetId=spreadsheet_id, range=tab_name)
+        write_request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=tab_name,
                                                                body={'values': tab_data}, valueInputOption='RAW')
 
         clear_request.execute()
